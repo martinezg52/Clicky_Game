3a3e082e565c6e7c2ec7f64cfff4bd53f1c9f151
geo_ep <geoffrey.xwin@gmail.com>
2019-09-05 01:27:08 -0700

    adding card jsx

---
+++ clicky_game/src/components/Card/card.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Card.css";
+
+//creating jsx to display the card image and name
+
+const Card = props => (
+  <div className="card" onClick={() => props.clickCount(props.id)}>
+    <div className="img-container">
+      <img alt={props.name} src={props.image} />
+    </div>
+  </div>
+);
+
+export default Card;
