4ee9564532a90f38e7139732b73992d859f65e15
geo_ep <geoffrey.xwin@gmail.com>
2019-09-05 01:55:17 -0700

    app is working now

---
+++ clicky_game/src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+//Adding header jsx
+
+const Header = props => (
+  <div className="header">
+    <div className="title">{props.children}</div>
+    <div className="scores">
+      Score: {props.score} Highscore: {props.highscore}
+    </div>
+  </div>
+);
+
+export default Header;
